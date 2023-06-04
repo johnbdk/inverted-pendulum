cb68f88478073db9bc05f415bdb92d10cf7ff0e5
Giannis Badakis <i.badakis@student.tue.nl>
2023-06-04 12:35:18 +0200

    refactor main parser

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # inverted-pendulum
 This project is about implemented a reinforcement learning approach in combination with artificial neural networks (ANN) and Gaussian processes (GP) to stabilize an inverted pendulum at a target position
 
-Eg. to train ANN:
+Eg. to train ANN with architecture noe:
 ```bash
-python .\main.py method --name ann --train 
+python .\main.py method --ann --train --model-arch noe
 ```
+
+Eg. to test GP with architecture narx:
+```bash
+python .\main.py method --gp --test --model-arch narx   
+```
+
+Eg. to test ANN with architecture ss and specific model:
+```bash
+python .\main.py method --gp --test --model-arch narx --model-file state_space.pth 
+```
+
+Eg. to train ANN with architecture narx and display detailed processing information on your screen with the flag verbose:
+```bash
+python .\main.py method --ann --train --model-arch narx --verbose
+```
+
+Eg. to RE-train ANN with architecture narx and display detailed processing information on your screen with the flag verbose:
+```bash
+python .\main.py method --ann --train --model-arch narx --model-path narx.pth --verbose
+```
+
+As you can see, on the last example we spacified the model name also. This is because we, first, need to load the model, save its current state and continue its training.
--- src/main.py
+++ src/main.py
@@ -10,20 +10,6 @@ from gp.model import PendulumManager
 
 ###########  P A R A M E T E R S  ###########
 
-# Define Arguments
-parser = argparse.ArgumentParser()
-
-# Usual arguments which are applicable for the whole script / top-level args
-parser.add_argument('--verbose', help='Common top-level parameter',
-                    action='store_true', required=False)
-
-## General Arguments
-parser.add_argument('--model-file', type = str, default = os.path.join(MODELS_DIR, "model.pth"),
-                           required=False, help = 'Specify the model path to be stored/loaded')
-parser.add_argument('--model-arch', type=str, choices=['narx, noe, ss'], default='narx',
-                           required=False, help='Choose model architecture')
-
-subparsers = parser.add_subparsers(title="actions")
 
 
 # PARENT PARSER FOR SOME SUBPARSERS TO INHERIT THE ARGUMENTS IN ORDER TO BE SHARED, THUS WE DONT NEED
@@ -31,35 +17,44 @@ subparsers = parser.add_subparsers(title="actions")
 parent_parser = argparse.ArgumentParser(description = 'put some description here...', add_help=False)
 parent_parser.add_argument('--train', action='store_true', default=False, help='Train the model')
 parent_parser.add_argument('--test', action='store_true', default=False, help='Train the model')
+parent_parser.add_argument('--verbose', help='Common top-level parameter',
+                    action='store_true', required=False)
+# parent_parser.add_argument("--action", action='store_true', help="action of the mode to be used")
+# parent_parser.add_argument('--save', action='store_true', default=False, help='Save the model')
 
+## General Arguments
+parent_parser.add_argument('--model-file', type = str, default = os.path.join(MODELS_DIR, "model.pth"),
+                           required=False, help = 'Specify the model path to be stored/loaded')
+parent_parser.add_argument('--model-arch', type=str, choices=['narx', 'noe', 'ss'], default='narx',
+                           required=False, help='Choose model architecture')
 
+# Define Arguments
+parser = argparse.ArgumentParser(parents=[parent_parser])
+subparsers = parser.add_subparsers(title="actions")
 parser_method = subparsers.add_parser("method", parents=[parent_parser],
                                       description='The method parser', help='Method to be chosen (ANN or GP)')
-parser_method.add_argument('--name', type = str, default="ann", help="name of the method to be used")
+parser_method.add_argument('--gp', action='store_true', default=False, help="name of the method to be used")
+parser_method.add_argument('--ann', action='store_true', default=False, help="name of the method to be used")
 
 # parser_mode = subparsers.add_parser("mode", parents=[parent_parser], add_help=False,
 #                                       description="The mode parser", help="Mode to be chosen (Train or Test)")
-# parser_mode.add_argument("--action", action='store_true', help="action of the mode to be used")
-# parser_mode.add_argument('--save', action='store_true', default=False, help='Save the model')
-
-subparsers.required = True
 
 def __main__():
     # Compile Arguments
     args = parser.parse_args()
-    # print(parser.print_help())
+    print(parser.print_help())
     print(args)
 
     if args.train:
-        if args.name == 'gp':
+        if args.gp:
             PendulumManager()
-        elif args.name == 'ann':
+        elif args.ann:
             train_ann()
             print("Training of the model has been completed")
     elif args.test:
-        if args.name == 'gp':
+        if args.gp:
             PendulumManager()
-        elif args.name == 'ann':
+        elif args.ann:
             if eval_ann(args.mode_path, args.model_arch):
                 print("Evaluation of the model has been completed")
             else:
